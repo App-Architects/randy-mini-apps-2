60c6841e3bf305b64ec6c47c3de9b0ec5d4605f9
Randy <rlthomasccv@gmail.com>
2019-03-29 15:07:01 -0700

    Grabbing API data and sending to chart

--- challenge_2/client/src/app.js
+++ challenge_2/client/src/app.js
@@ -1,3 +1,6 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable react/jsx-filename-extension */
@@ -7,17 +10,37 @@ import React, { Component } from 'react';
 import Modal from 'react-awesome-modal';
 import $ from 'jquery';
 import Axios from 'axios';
+import Chart from './components/Chart';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      chartData: [],
+    };
   }
 
+  componentWillMount() {
+    Axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-02-01&end=2019-09-05')
+      .then((res) => {
+        const arrData = [];
+        $.each(res.data.bpi, (k, v) => {
+          arrData.push({ date: k, close: v });
+        });
+        this.setState({
+          chartData: arrData,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   render() {
+    const { chartData } = this.state;
     return (
       <div>
-        <h1>HELLO WORLD</h1>
+        <Chart chartData={chartData} />
       </div>
     );
   }
---
+++ challenge_2/client/src/components/Chart.js
@@ -0,0 +1,76 @@
+/* eslint-disable prefer-const */
+/* eslint-disable quote-props */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  chart() {
+    const { chartData } = this.props;
+    const months = {
+      '01': 'Jan',
+      '02': 'Feb',
+      '03': 'Mar',
+      '04': 'Apr',
+      '05': 'May',
+      '06': 'Jun',
+      '07': 'Jul',
+      '08': 'Aug',
+      '09': 'Sep',
+      '10': 'Oct',
+      '11': 'Nov',
+      '12': 'Dec',
+    };
+    let data = {
+      labels: [],
+      legend: false,
+      datasets: [
+        {
+          label: 'Closing Price',
+          data: [],
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgba(232, 44, 12)',
+          pointBackgroundColor: 'rgba(232, 44, 12)',
+          borderWidth: 2,
+          lineTension: 0,
+        },
+      ],
+    };
+    console.log(chartData, 'PROPS');
+    chartData.map((elem) => {
+      const date = elem.date.slice(5);
+      const month = date.substring(0, 2);
+      const day = date.substring(3, 5);
+      data.labels.push(`${months[month]} ${day}`);
+      data.datasets[0].data.push(elem.close);
+    });
+    return (
+      <div>
+        <Line
+          data={data}
+          height={500}
+          options={{
+            maintainAspectRatio: false,
+            elements: { point: { radius: 0 } },
+          }
+            }
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      this.chart()
+    );
+  }
+}
+
+export default Chart;
