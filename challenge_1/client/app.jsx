36b449a0149bf0530c61a76817eaf9f5215314ca
Randy <rlthomasccv@gmail.com>
2019-04-01 18:35:56 -0700

    Initiate Repo

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,17 @@
+import ReactPaginate from 'react-paginate';
+import React, { Component } from 'react';
 
 
+
+class App extends Component {
+    
+    render() { 
+        return (
+            <div>
+                <h1>HELLO WORLD</h1>
+            </div>
+        );
+    }
+}
+ 
+export default App;
