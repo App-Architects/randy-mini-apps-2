f99f4414bc89654163ab45fedd031acf088ab522
Randy <rlthomasccv@gmail.com>
2019-04-01 20:40:24 -0700

    Seeding api data to client

--- challenge_1/src/client/App.js
+++ challenge_1/src/client/App.js
@@ -14,7 +14,9 @@ class App extends Component {
         console.log(e.target.query.value)
         Axios.get(`/events?q=${e.target.query.value}`)
         .then(res => {
-            console.log(res.data);
+            this.setState({
+                data: res.data
+            })
         })
         .catch(err => {
             console.log(err);
@@ -23,22 +25,46 @@ class App extends Component {
 
     form() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <div className="form ">
+            <form onSubmit={this.handleSubmit.bind(this)}>
                 <div className="form-group">
-                <div className="col-sm-3">
-                    <label>Keyword Search</label>
+                    <label>History Search</label>
                     <input type="text" id="query" className="form-control" placeholder="Egypt" />
+                </div>
+                <div>
                     <button type="submit" className="btn btn-primary">Search</button>
                 </div>
-                </div>
             </form>
+            <br />
+            <hr></hr>
+            </div>
         )
     }
 
+    list() {
+        const { data } = this.state;
+        return (
+            <div className="list">
+                <ul className="list-group list-group-flush">
+                {data.map((elem, i) => {
+                    return <li className="list-group-item">
+                        <p><b>Year:</b> {elem.date} </p>
+                        <p><b>Location:</b> {elem.category2}</p>
+                        <p><b>Description:</b> {elem.description}</p>
+                    </li>
+                })}
+                </ul>
+            </div>
+            )
+    }
+
     render() { 
+        const { data } = this.state;
+        console.log(data, 'STATE')
         return (
             <div>
                 {this.form()}
+                {this.list()}
             </div>
         );
     }
